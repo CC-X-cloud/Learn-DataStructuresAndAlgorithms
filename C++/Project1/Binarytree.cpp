e28f1cac4e147f43a04696a0ee12126a8077fa63
CC-X-cloud <3415422026@qq.com>
2026-09-22 16:48:20 +0800

    二叉树和图结构

--- C++/Project1/Binarytree.cpp
+++ C++/Project1/Binarytree.cpp
@@ -1,5 +1,8 @@
 ﻿#include <iostream>
 #include <vector>
+#include <stack>
+#include <queue>
+#include <unordered_map>
 
 using namespace std;
 
@@ -30,4 +33,227 @@ public:
 	}
 
 	//代码模拟前中后序遍历
+	vector<int> preorderTraversal(TreeNode* root) {
+		vector<int> res;
+		if (!root) return res;
+
+		std::stack<TreeNode*> stk;
+		stk.push(root);
+
+		while (!stk.empty()) {
+			TreeNode* node = stk.top();
+			stk.pop();
+			res.push_back(node->val); // 前序遍历，先访问根节点
+
+			if (node->right) stk.push(node->right); // 先右后左，保证左子树先被访问
+			if (node->left) stk.push(node->left);
+		}
+		return res;
+	}
+	vector<int> inorderTraversal(TreeNode* root) {
+		vector<int> res;
+		std::stack<TreeNode*> st;
+		TreeNode* curr = root;
+		while (curr != nullptr || !st.empty()) {
+			while (curr != nullptr) {
+				st.push(curr);
+				curr = curr->left;
+			}
+			curr = st.top();
+			st.pop();
+			res.push_back(curr->val); // 中序遍历，访问根节点
+			curr = curr->right;
+		}
+		return res;
+	}
+	vector<int> postorderTraversal(TreeNode* root) {
+		vector<int> res;
+		if (!root) return res;
+		std::stack<TreeNode*> st;
+
+		TreeNode* curr = root;
+		TreeNode* prev = nullptr;
+
+		while (curr != nullptr || !st.empty()) {
+			while (curr != nullptr) {
+				st.push(curr);
+				curr = curr->left;
+			}
+
+			curr = st.top();
+
+			if(curr -> right == nullptr || curr->right == prev) {
+				res.push_back(curr->val); // 后序遍历，访问根节点
+				st.pop();
+				prev = curr;
+				curr = nullptr; // 防止再次进入左子树
+			} else {
+				curr = curr->right;
+			}
+		}
+		return res;
+	}
+
+	//层序遍历
+	vector<int> levelOrder(TreeNode* root) {
+		vector<int> res;
+		if (root == nullptr) return res;
+
+		std::queue<TreeNode*> q;
+		q.push(root);
+
+		while (!q.empty()) {
+			TreeNode* node = q.front();
+			q.pop();
+			res.push_back(node->val);
+
+			if (node->left) q.push(node->left);
+			if (node->right) q.push(node->right);
+		}
+		return res;
+	}
+	//层序遍历按层输出
+	vector<vector<int>> levelOrderByLevel(TreeNode* root) {
+		vector<vector<int>> res;
+		if (root == nullptr) return res;
+
+		std::queue<TreeNode*> q;
+		q.push(root);
+		while (q.empty()) {
+			
+			vector<int> level;
+			int size = q.size();
+
+			for(int i= 0;i< size;i++) {
+				TreeNode* node = q.front();
+				q.pop();
+				level.push_back(node->val);
+				if (node->left) q.push(node->left);
+				if (node->right) q.push(node->right);
+			}
+			res.push_back(level);
+
+		}
+		return res;
+	}
+	//层序遍历哈希表存储
+	vector<vector<int>> levelOrderByHash(TreeNode* root) {
+		std::vector<std::vector<int>> result;
+		if (!root) return result;
+
+		std::queue<TreeNode*> q;
+		std::unordered_map<TreeNode*, int> depthMap; // 1. 哈希表：节点指针 -> 深度
+
+		q.push(root);
+		depthMap[root] = 0; // 根节点深度为 0
+
+		while (!q.empty()) {
+			TreeNode* node = q.front();
+			q.pop();
+
+			int depth = depthMap[node]; // 2. 从哈希表获取当前节点深度
+
+			// 3. 如果 result 还没有这一层的容器，则创建一个新的
+			if (result.size() == depth) {
+				result.push_back(std::vector<int>());
+			}
+			result[depth].push_back(node->val); // 4. 放入对应层的数组中
+
+			// 5. 子节点入队，并在哈希表中记录其深度为 父节点深度 + 1
+			if (node->left) {
+				depthMap[node->left] = depth + 1;
+				q.push(node->left);
+			}
+			if (node->right) {
+				depthMap[node->right] = depth + 1;
+				q.push(node->right);
+			}
+		}
+		return result;
+	}
+	//层序遍历滚动更新
+	vector<vector<int>> levelOrderByRollingUpdate(TreeNode* root) {
+		vector<vector<int>> res;
+		if (root == nullptr) return res;
+
+		TreeNode* curEnd = root; // 当前层的最后一个节点
+		TreeNode* nextEnd = nullptr; // 下一层的最后一个节点
+
+		std::queue<TreeNode*> q;
+		q.push(root);
+		
+		while (!q.empty()) {
+			std::vector<int> level;
+			
+			TreeNode* node = q.front();
+			q.pop();
+			level.push_back(node->val);
+
+			if (node->left) {
+				q.push(node->left);
+				nextEnd = node->left; // 更新下一层的最后一个节点
+			}
+			if(node -> right) {
+				q.push(node->right);
+				nextEnd = node->right; // 更新下一层的最后一个节点
+			}
+			if (node == curEnd) {
+				res.push_back(level);
+				curEnd = nextEnd;
+			}
+		}
+		return res;
+	}
+	//搜索二叉树判断
+	bool isVailTree(TreeNode* root) {
+		vector<long long> vals;
+		inorder(vals, root);
+		int size = vals.size();
+		for (int i = 0; i < size - 1; i++) {
+			if (vals[i] > vals[i + 1]) {
+				return false;
+			}
+		}
+		return true;
+	}
+	void inorder(vector<long long>& vals,TreeNode* root) {
+		if (root == nullptr) return;
+
+		inorder(vals,root->left);
+		vals.push_back(root->val);
+		inorder(vals, root->right);
+	}
+	bool inorderv2(TreeNode* root) {
+		if (root == nullptr) return true;
+		TreeNode* pre = nullptr;
+		if(!inorderv2(root->left)) return false;
+		if (pre != nullptr && pre->val > root->val) {
+			return false;
+		}
+		pre = root;
+		inorderv2(root->right);
+	}
+	bool inorderv3(TreeNode* root) {
+		std::stack<TreeNode*> st;
+		st.push(root);
+
+		TreeNode* cur = root;
+		TreeNode* pre = nullptr;
+		while (!st.empty()) {
+			while (!cur) {
+				st.push(cur);
+				cur = cur->left;
+			}
+			cur = st.top();
+			st.pop();
+			//处理
+			if (!pre && pre->val > cur->val) {
+				return false;
+			}
+			pre = cur;
+			cur = cur->right;
+		}
+		return true;
+	}
+
 };
---
+++ C++/Project1/Graph.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+struct Node;
+struct Edge;
+
+struct Node {
+	int value;
+	int in;
+	int out;
+	std::vector<shared_ptr<Node>> nodes;
+	std::vector<shared_ptr<Edge>> edges;
+	Node(int val) : value(val), in(0), out(0) {}
+};
+
+struct Edge {
+	shared_ptr<Node> from;
+	shared_ptr<Node> to;
+	int weight;
+	Edge(shared_ptr<Node> f, shared_ptr<Node> t, int w) : from(f), to(t), weight(w) {}
+};
+
+struct Graph {
+	std::unordered_map<int, shared_ptr<Node>> nodes;
+	std::unordered_set<shared_ptr<Edge>> edges;
+};
+
+
+class GraphAlgorithm {
+public:
+
+};
